be490ccc6a972130ff975e68c6e863016e787550
Tom Robertshaw <tom.robertshaw@meanbee.com>
2011-01-22 20:14:17 +0000

    Save downloads on each day so that we can show change over 7 days.  Clean up old files after us as well.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Magento Module Downloads Stats for Geckoboard
 
-Given a id of a Magento module (number can be seen in the url on the magento connect page), find the number of downloads and prepare for entry into Geckoboard.
+Given a id of a Magento module (number can be seen in the url on the magento connect page), find the number of downloads and prepare for entry into Geckoboard.  After a week of use, will show increase/decrease from previous week.
 
 e.g.  BASE_URL/module/downloads/480
 
--- downloads.php
+++ downloads.php
@@ -4,7 +4,11 @@
  * Retrieve download count from magentocommerce.com for a module
  */
 
+// Include geckoboard response class
 require_once('response.class.php');
+
+// Set date format to be nice
+date_default_timezone_set("UTC"); 
 
 // Check that we have the id of the module
 if(isset($_GET['id'])) {
@@ -16,16 +20,31 @@ if(isset($_GET['id'])) {
 
     // Find count and convert to geckoboard array
     $downloads = findDownloads($html);
-    $gecko = convertToGecko($downloads);
+
+    // write to file for use in a week
+    saveDownloads($module_id, $downloads);
+
+    // Delete old data
+    cleanOldDownloads($module_id);
+
+    // Get data from 7 days ago.
+    $priorDownloads = getLastWeeksDownloads($module_id);
+
+    // Delete data from 8 days ago
+    cleanOldDownloads($module_id);
+
+    // Put in array of values and items.
+    $gecko = convertToGecko($downloads, $priorDownloads);
 
     // Convert to xml/json as appropriate
     $format = isset($_POST['format']) ? (int)$_POST['format'] : 0;
     $format = ($format == 1) ? 'xml' : 'json';
     $response_obj = new Response();
     $response_obj->setFormat($format);
-
     $response = $response_obj->getResponse($gecko);
-    echo $response;  
+
+    echo $response;
+
 }
 
 /*
@@ -55,6 +74,44 @@ function findDownloads($html) {
 /*
  * Convert to array of values within an array of items
  */
-function convertToGecko($downloads) {
-    return array("item" => array("value" => $downloads));
+function convertToGecko($downloads, $priorDownloads) {
+    $gecko = array("item" => array( array("value" => $downloads)));
+
+    if($priorDownloads) {
+        $gecko['item'][] = array("value" => $priorDownloads);
+    }
+
+    return $gecko;
+}
+
+/*
+ * Save today's downloads, ready to be used in a week
+ */
+function saveDownloads($module_id, $downloads) {
+    $filename = "cache/$module_id-" . date("y-m-d") . ".txt";
+    if(!file_exists($filename)) {
+        file_put_contents($filename, $downloads);
+    }
+}
+
+/*
+ * Get last week's downloads
+ */
+function getLastWeeksDownloads($module_id) {
+    $filename = "cache/$module_id-" . date("y-m-d", time() - (7 * 86400)) . ".txt";
+    if(file_exists($filename)) {
+        return trim(file_get_contents($filename));
+    }
+
+    return false;
+}
+
+/*
+ * Clean up old files.
+ */
+function cleanOldDownloads($module_id) {
+    $filename = "cache/$module_id-" . date("y-m-d", time() - (8 * 86400)) . ".txt";
+    if(file_exists($filename)) {
+        unlink($filename);
+    }
 }
